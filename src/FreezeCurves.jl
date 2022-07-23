c4b484b40087b9c4a005c6ad8cc835de8b49d398
Brian Groenke <brian.groenke@awi.de>
2022-07-23 12:32:53 +0200

    Fix inconsistent argument order for FreeWater

--- src/FreezeCurves.jl
+++ src/FreezeCurves.jl
@@ -24,26 +24,6 @@ Base type for freezing characteristic curves which relate temperature or enthalp
 """
 abstract type FreezeCurve end
 
-# Free water freeze curve
-"""
-    FreeWater <: FreezeCurve
-
-"Free water" freeze curve in terms of enthalpy (H), total water content (θtot), and
-the latent heat of fusion of water (L).
-"""
-struct FreeWater <: FreezeCurve end
-function freewater(H, L, θtot)
-    θtot = max(1e-8, θtot)
-    Lθ = L*θtot
-    I_t = H > Lθ
-    I_f = H <= 0.0
-    I_c = (H > 0.0) && (H <= Lθ)
-    # compute liquid water content -> heat capacity -> temperature
-    θw = (I_c*(H/Lθ) + I_t)θtot
-    return (;θw, I_t, I_f, I_c, Lθ)
-end
-(freeW::FreeWater)(H, θtot, L) = freewater(H, θtot, L).θw
-
 """
     enthalpy(T, C, L, θw)
 
@@ -66,6 +46,26 @@ export SFCCFunction, SFCCSolver, SoilFreezeThawProperties, DallAmico, DallAmicoS
 include("sfcc.jl")
 include("Solvers/Solvers.jl")
 using .Solvers
+
+# Free water freeze curve
+"""
+    FreeWater <: FreezeCurve
+
+"Free water" freeze curve in terms of enthalpy (H), total water content (θtot), and
+the latent heat of fusion of water (L).
+"""
+struct FreeWater <: FreezeCurve end
+function freewater(H, θtot, L)
+    θtot = max(1e-8, θtot)
+    Lθ = L*θtot
+    I_t = H > Lθ
+    I_f = H <= 0.0
+    I_c = (H > 0.0) && (H <= Lθ)
+    # compute liquid water content -> heat capacity -> temperature
+    θw = (I_c*(H/Lθ) + I_t)θtot
+    return (;θw, I_t, I_f, I_c, Lθ)
+end
+(freeW::FreeWater)(H, θtot, L) = freewater(H, θtot, L).θw
 
 """
     SFCC{F,S} <: FreezeCurve
