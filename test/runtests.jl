3eed0d7f587835a875b65ecb1240ab64d7239949
Brian Groenke <brian.groenke@awi.de>
2023-08-08 15:17:50 +0200

    Rename new test file to follow convention

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 using Unitful
 
 @testset "FreezeCurves.jl" begin
+    include("freewater_tests.jl")
     include("swrc_tests.jl")
     include("sfcc_tests.jl")
     include("solver_tests.jl")
